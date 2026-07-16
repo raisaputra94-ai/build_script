68779cd0cec69512382d8c5759596f23cc4eea61
raisaputra94-ai <raisaputra94@gmail.com>
2026-07-16 09:27:16 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 set -e
 
-# Init official LineageOS 18.1 (matches Project 85, so no issues)
-repo init -u https://github.com/LineageOS/android.git \
-    -b lineage-18.1 \
-    --depth=1
-
-# Clone local manifest
-rm -rf .repo/local_manifests
-git clone -q https://github.com/raisaputra94-ai/local_manifests.git --depth 1 .repo/local_manifests
-
-# Sync
-/opt/crave/resync.sh
-
-# Build
-source build/envsetup.sh
-lunch lineage_RMX1805-user
-
 export BUILD_HOSTNAME=android-build
 export BUILD_USERNAME=rai
 export TZ=Asia/Singapore
 
+echo "start date = `date`"
+# Init official LineageOS 18.1 (matches Project 85, so no issues)
+repo init --depth 1 -u https://github.com/LineageOS/android -b lineage-18.1 --git-lfs
+
+echo "clone manifest = `date`"
+# Clone local manifest
+rm -rf .repo/local_manifests
+git clone -q https://github.com/raisaputra94-ai/local_manifests.git --depth 1 .repo/local_manifests
+
+echo "sync = `date`"
+# Sync
+/opt/crave/resync.sh
+
+echo "lib = `date`"
+#
+FILE="/usr/lib/x86_64-linux-gnu/libncurses.so.5"
+if [ ! -f "$FILE" ]; then
+  echo "File '$FILE' does not exist."
+  sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5 
+  sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6 /usr/lib/x86_64-linux-gnu/libtinfo.so.5 
+fi
+
+echo "build = `date`"
+# Build
+source build/envsetup.sh
+lunch lineage_RMX1805-user
 mka bacon
