f489af8d5b1c1dccbd675892070aad2fe37f2484
raisaputra94-ai <raisaputra94@gmail.com>
2026-07-17 19:20:01 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ export BUILD_HOSTNAME=android-build
 export BUILD_USERNAME=rai
 export TZ=Asia/Singapore
 
+
+
 echo "start date = `date`"
 # Init official LineageOS 18.1 (matches Project 85, so no issues)
 repo init --depth 1 -u https://github.com/LineageOS/android -b lineage-18.1 --git-lfs
@@ -20,13 +22,18 @@ echo "sync = `date`"
 /opt/crave/resync.sh
 
 echo "lib = `date`"
+wget -q https://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.3-2_amd64.deb && \
+    sudo dpkg -i libtinfo5_6.3-2_amd64.deb && rm -f libtinfo5_6.3-2_amd64.deb || true
+wget -q https://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libncurses5_6.3-2_amd64.deb && \
+    sudo dpkg -i libncurses5_6.3-2_amd64.deb && rm -f libncurses5_6.3-2_amd64.deb || true
+
 #
-FILE="/usr/lib/x86_64-linux-gnu/libncurses.so.5"
-if [ ! -f "$FILE" ]; then
-  echo "File '$FILE' does not exist."
-  sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5 
-  sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6 /usr/lib/x86_64-linux-gnu/libtinfo.so.5 
-fi
+# FILE="/usr/lib/x86_64-linux-gnu/libncurses.so.5"
+# if [ ! -f "$FILE" ]; then
+#   echo "File '$FILE' does not exist."
+#   sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5 
+#   sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6 /usr/lib/x86_64-linux-gnu/libtinfo.so.5 
+# fi
 
 echo "build = `date`"
 # Build
