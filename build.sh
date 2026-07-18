98dc293f95ad9ba0f94aa52685ad2e43f7a4ffe8
raisaputra94-ai <raisaputra94@gmail.com>
2026-07-18 22:54:55 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,24 +5,19 @@ export BUILD_HOSTNAME=android-build
 export BUILD_USERNAME=rai
 export TZ=Asia/Singapore
 
-# Install libncurses5
-wget -q https://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.3-2_amd64.deb && \
-    sudo dpkg -i libtinfo5_6.3-2_amd64.deb && rm -f libtinfo5_6.3-2_amd64.deb || true
-wget -q https://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libncurses5_6.3-2_amd64.deb && \
-    sudo dpkg -i libncurses5_6.3-2_amd64.deb && rm -f libncurses5_6.3-2_amd64.deb || true
-
-# Fix: delete CORRUPTED manifests repo specifically
-rm -rf .repo/manifests
+# Remove old local manifests
 rm -rf .repo/local_manifests
+mkdir -p .repo/local_manifests
 
-# Re-init (will re-clone manifests git repo fresh)
-repo init -u https://github.com/LineageOS/android.git \
-    -b lineage-18.1 \
-    --depth=1 \
-    --git-lfs
-
-# Add local manifests
-git clone -q https://github.com/raisaputra94-ai/local_manifests.git --depth 1 .repo/local_manifests
+# Create manifest with renamed remote (avoids "github" conflict)
+cat > .repo/local_manifests/rmx1805.xml << 'XMLEOF'
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+  <remote name="rmx1805" fetch="https://github.com/" />
+  <project name="RMX1805/device_oppo_RMX1805" path="device/oppo/RMX1805" remote="rmx1805" revision="lineage-18.1" />
+  <project name="RMX1805/vendor_oppo" path="vendor/oppo" remote="rmx1805" revision="lineage-18.1" />
+</manifest>
+XMLEOF
 
 # Sync
 /opt/crave/resync.sh
